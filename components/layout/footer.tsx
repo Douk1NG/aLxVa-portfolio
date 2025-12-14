d2065075423316de59847d1f6dc2d7f263b4ef5a
aLxVa <dialexanderx@gmail.com>
2025-12-14 23:36:47 +0100

    ajuste footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,14 +1,9 @@
-'use client'
-
-import { useHeartColor } from '@/hooks/useHeartColor'
-
 export function Footer() {
-  const { heart } = useHeartColor()
 
   return (
     <footer>
       <p className="mx-auto max-w-7xl text-center text-muted-foreground">
-        Built with {heart} and IA
+        Built with ❤️ and IA
       </p>
     </footer>
   )
